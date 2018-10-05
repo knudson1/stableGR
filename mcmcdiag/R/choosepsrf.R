2fd1fbf2daec1d962a20567ef24b40ea2857cdfd
Christina Knudson UST Office <knud8583@stthomas.edu>
2018-10-05 15:32:28 -0500

    updating to remove df, V; make lug default

--- mcmcdiag/R/choosepsrf.R
+++ mcmcdiag/R/choosepsrf.R
@@ -1,15 +1,21 @@
-# Calculates the sqrt(R) = GR diagnostic threshold (arr):
-# When the sample diagnostic gets to arr, the chain has converged
-# p:dimension of the estimation problem
-# m: number of chains
-# epsilon: relative precision level
-# alpha: significance level
+#' Calculates a Gelman Rubin diagnostic threshold
+#' When the sample diagnostic reaches this threshold, the chain has converged.
+#' @param p dimension of the estimation problem
+#' @param m number of chains
+#' @param epsilon relative precision level
+#' @param alpha significance level
+#' @return The psrf threshold to be reached for convergence.
 
-minESS <- mcmcse::minESS
-
-findarr <- function(p, epsilon, m, alpha=.05){
+choosepsrf <- function(p, epsilon, m, alpha=.05){
   Tee <- as.numeric(minESS(p, epsilon, alpha = alpha)) #min effective sample size
   del <- sqrt(1 + m/Tee) - 1
   arr <- 1 + del  
   arr
 }
+
+minESS <- mcmcse::minESS
+
+
+
+
+
--- mcmcdiag/R/gelman.bm.R
+++ mcmcdiag/R/gelman.bm.R
@@ -1,6 +1,6 @@
 #####################################
-### Function gelman.bm incorporates batch means variance estimates
-### into the Gelman-Rubin diagnostic framework 
+### Incorporates lugsail batch means variance 
+### estimatesinto the Gelman-Rubin diagnostic framework 
 ### x: a list of chains
 ### the rest of the arguments are leftover from gelman.diag
 ### Returns the potential scale reduction factor (psrf) 
@@ -8,9 +8,9 @@
 #####################################
 
 gelman.bm <-
-function (x, confidence = 0.95, transform = FALSE, df = TRUE,  
+function (x, confidence = 0.95, transform = FALSE,  
     mapping = "determinant", autoburnin = FALSE, 
-    multivariate = TRUE, method = "bm", blather = FALSE) 
+    multivariate = TRUE, method = "lug", blather = FALSE) 
 {
     x <- as.mcmc.list(x)
     if (autoburnin && start(x) < end(x)/2) 
@@ -52,43 +52,7 @@ function (x, confidence = 0.95, transform = FALSE, df = TRUE,
 	# Calculate the estimate of sigma^2.
 	sigsq <- (Niter - 1) * Ssq/Niter + tau2 / Niter 
 
-	# Calculate V, the scale of the t distribution.    
-	V <- sigsq +  tau2/(Niter * Nchain) 
-
-	df.adj <- 1
-
-    if(Nchain > 1 & df){
-
-		tau2var <- apply(tau2i, 1, var)/Nchain # Calculate the variance of our estimate
-
-		# Calculate cov(tau^2, s^2). Calculate it for each chain, then average.
-		# This replaces cov(w,b) from GR. w = s^2 but tau^2 != b.
-		cov.s2t2 <- sapply(1:Nvar, function(index) {cov(x = tau2i[index, ], y = s2[index, ])}) 
-		# cov(s^2, tau^2) = sample cov(s^2, tau^2) 
-
-
-
-		# Variance of the variances, divided by m chains. 
-		var.s2 <- apply(s2, 1, var)/Nchain # Known as var.w in GR.
-
-
-	    # Calculate the variance of V.
-		var.V <- ((Niter - 1)/Niter)^2 * var.s2 + 
-			+ ((Nchain + 1)/(Nchain * Niter))^2 * tau2var  +  
-			+ 2 * (Nchain + 1) * (Niter -1) * cov.s2t2 / (Nchain^2 * Niter^2)
-
-		# Eight, calculate degrees of freedom for our T dist.
-
-		df.V <- (2 * V^2)/var.V 
-   		df.adj <- (df.V + 3)/(df.V + 1) 
-		# Adjustment keeps SRF finite. 
-	}
-
-
-    #if(df == FALSE) {df.adj <- 1}
-
-
-	arrr <- V * df.adj / Ssq
+	arrr <- sigsq / Ssq
 	psrf <- sqrt(arrr)
 
 	if(multivariate == TRUE  && Nvar > 1){
@@ -119,7 +83,7 @@ function (x, confidence = 0.95, transform = FALSE, df = TRUE,
 
 	if(blather){
 		blather <- list(muhat = muhat, method = method, Niter = Niter, Nchain = Nchain, Nvar = Nvar,
-			tausq = tau2, ssq <- s2, sigsq = sigsq, df.adj = df.adj, S = W, Tee = Tee)
+			tausq = tau2, ssq <- s2, sigsq = sigsq, S = W, Tee = Tee)
 	}
 
 	list(psrf = psrf, mpsrf = mpsrf, blather = blather)
