33fe2d8aebe79876b7cb8e1ab08a5c972cbdb7f2
Christina Knudson UST Office <knud8583@stthomas.edu>
2018-12-11 12:43:43 -0600

    changed name of choosepsrf to target.psrf

--- mcmcdiag/tests/testchoosepsrf.R
+++ mcmcdiag/tests/testchoosepsrf.R
@@ -8,7 +8,7 @@ m <- 5
 #### Check the case where delta is null (epsilon is instead given)
 # That is, find the effective sample size based on epsilon
 epsilon <- .05
-out1a <- choosepsrf(p, epsilon = epsilon, m = m, alpha = alpha)
+out1a <- target.psrf(p, epsilon = epsilon, m = m, alpha = alpha)
 Tee <- as.numeric(minESS(p, epsilon, alpha = alpha)) #min effective sample size
 del <- sqrt(1 + m/Tee) - 1
 arr <- 1 + del  
@@ -18,7 +18,7 @@ all.equal(out1a, out1b)
 #### Check the case where delta is NOT null (it's given)
 # That is, find the effective sample size based on delta
 delta <- .01
-out2a <- choosepsrf(p, delta = delta, m = m, alpha = alpha)
+out2a <- target.psrf(p, delta = delta, m = m, alpha = alpha)
 Tee <- M <- m/((1+delta)^2 - 1)
 epsilon <- as.numeric(minESS(p, eps = epsilon, ess = M))
 del <- sqrt(1 + m/Tee) - 1
