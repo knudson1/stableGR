efd53812df8427c99f2df16d6d9bce35ae74460f
Christina Knudson <knud8583@stthomas.edu>
2019-03-27 11:07:44 -0500

    added documentation for target psrf

--- mcmcdiag/R/target.psrf.R
+++ mcmcdiag/R/target.psrf.R
@@ -7,12 +7,22 @@
 #' @param delta desired delta value
 #' @param m number of chains
 #' @param alpha significance level
+#' 
 #' @return \item{psrf}{The psrf threshold to be reached for convergence.} 
 #' @return \item{epsilon}{The epsilon value used to calculate the psrf threshold.}
 #' @return \item{delta}{The delta value used to calculate the psrf threshold.}
 #' @examples target.psrf(1, delta = .1, m = 5, alpha = .05)
 #' @examples target.psrf(1, epsilon = .05, m = 5, alpha = .05)
-
+#' 
+#' @section References:
+#' Vats, D. and Knudson, C. Revisiting the Gelman-Rubin Diagnostic.	arXiv:1812.09384 
+#' 
+#' Flegal, J. M. and Jones, G. L. (2010) Batch means and spectral variance estimators in Markov chain Monte Carlo. \emph{The Annals of Statistics}, \bold{38}, 1034--1070. \cr
+#' Gelman, A and Rubin, DB (1992) Inference from iterative simulation using multiple sequences, \emph{Statistical Science}, \bold{7}, 457-511. \cr
+#' Brooks, SP. and Gelman, A. (1998) General methods for monitoring convergence of iterative simulations. \emph{Journal of Computational and Graphical Statistics}, \bold{7}, 434-455.
+#'
+#' @export
+#'
 
 target.psrf <- function(p, epsilon = .05, delta = NULL, m, alpha=.05){
   if(is.null(delta)){
