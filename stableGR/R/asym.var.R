c6bf5c0caba5fcc6a06274c8e13f7871c141d9cb
Christina Knudson <knud8583@stthomas.edu>
2021-09-22 20:24:27 -0500

    working on asym.var, new size and trim
    Added a new function that calculates the ideal batch size for
    replicated bm and then trims the first few iterations off the chains, as
    necessary

--- stableGR/R/asym.var.R
+++ stableGR/R/asym.var.R
@@ -52,57 +52,15 @@ asym.var <- function (x, multivariate = TRUE, method = "lug", size = NULL, autob
   # perform various checks on markov chains
   x <- mcmcchecks(x, autoburnin = autoburnin)
   
-  # Define some notation
-  Nchain <- length(x)
-  
-  if(is.vector(x[[1]])) #single component
-  {
-    Nvar <- 1
-    Niter <- length(x[[1]])
-    }
-  if(is.matrix(x[[1]])) #univariate OR multivariate
-    {
-    Nvar <- ncol(x[[1]])
-    Niter <- nrow(x[[1]])  # number of iterations per chains. We also call this n.
-  } # number of variables
-  
-  # calculate batch size and number of batches based on user input.
-  # When we have multiple chains, we need to do replicated batch means
-  # meaning we need to calculate the batch sizes manually
-  if(is.null(size)){
-    bvec <- sapply(x, FUN = batchSize, simplify = TRUE, method = "bm")  
-    b <- floor(mean(bvec))
-    a <- floor(Niter/b)
-  }
-  if(is.null(size) == FALSE){    # if size != NULL
-    if (size == "sqroot") {   #calculation for square root
-        b = floor(sqrt(Niter))
-        a = floor(Niter/b)
-    }  else if (size == "cuberoot") { #calculation for cube root
-        b = floor(Niter^(1/3))
-        a = floor(Niter/b)
-    }  else {
-        if (!is.numeric(size) || size <= 1 || size == Inf) 
-            stop("'size' must be a finite numeric quantity larger than 1.")
-        b = floor(size) #calculation for a user-specified batch size
-        a = floor(Niter/b)
-    }
-  }
-  
-  ## trim away beginnings of each chain (if necessary)
-  Nneeded <- a*b
-  Ntrim <- Niter - Nneeded
-  trim2 <- x
-  if(Ntrim > 0){
-    removethese <- 1:Ntrim
-    # for(i in 1:Nchain){
-    #   trimmedchain[[i]] <- trimchain(x[[i]], removethese)
-    # }
-    trim2 <- lapply(x, FUN = trimchain, removethese = removethese)
-  }
-  
+  #trim the chains to prep for replicated bm
+  out <- size.and.trim(x = x, size = size)
+  b <- out$b
+  Nneeded <- out$Nneeded
+  trimmedchains <- out$trimmedchains
+  Nvar <- out$Nvar
+    
   ## stack the chains into a single matrix
-  stackedchains <- do.call(rbind,trim2)
+  stackedchains <- do.call(rbind, trimmedchains)
   
   ## calculate tau squared using replicated batch means
   if(multivariate == FALSE){
@@ -121,8 +79,5 @@ asym.var <- function (x, multivariate = TRUE, method = "lug", size = NULL, autob
 }
 
 
-trimchain <- function(fullchain, removethese){
-  Nvar <- ncol(fullchain)
-  matrix(fullchain[-removethese,], ncol = Nvar)
-}
+
 
---
+++ stableGR/R/size.and.trim.R
@@ -0,0 +1,60 @@
+
+size.and.trim <- function(x, size){
+    
+    # Define some notation
+    Nchain <- length(x)
+    
+    if(is.vector(x[[1]])) #single component
+    {
+        Nvar <- 1
+        Niter <- length(x[[1]])
+    }
+    if(is.matrix(x[[1]])) #univariate OR multivariate
+    {
+        Nvar <- ncol(x[[1]])
+        Niter <- nrow(x[[1]])  # number of iterations per chains. We also call this n.
+    } # number of variables
+    
+        
+        
+    if(is.null(size)){
+        bvec <- sapply(x, FUN = batchSize, simplify = TRUE, method = "bm")  
+        b <- floor(mean(bvec))
+        a <- floor(Niter/b)
+    }
+    if(is.null(size) == FALSE){    # if size != NULL
+        if (size == "sqroot") {   #calculation for square root
+            b = floor(sqrt(Niter))
+            a = floor(Niter/b)
+        }  else if (size == "cuberoot") { #calculation for cube root
+            b = floor(Niter^(1/3))
+            a = floor(Niter/b)
+        }  else {
+            if (!is.numeric(size) || size <= 1 || size == Inf) 
+                stop("'size' must be a finite numeric quantity larger than 1.")
+            b = floor(size) #calculation for a user-specified batch size
+            a = floor(Niter/b)
+        }
+    }
+    
+    ## trim away beginnings of each chain (if necessary)
+    Nneeded <- a*b
+    Ntrim <- Niter - Nneeded
+    trim2 <- x
+    if(Ntrim > 0){
+        removethese <- 1:Ntrim
+        # for(i in 1:Nchain){
+        #   trimmedchain[[i]] <- trimchain(x[[i]], removethese)
+        # }
+        trim2 <- lapply(x, FUN = trimchain, removethese = removethese)
+    }
+    
+    #return the size info and the list of trimmed chains
+    list(a = a, b = b, Nneeded = Nneeded, Nvar = Nvar, trimmedchains = trim2) 
+    
+}
+
+trimchain <- function(fullchain, removethese){
+    Nvar <- ncol(fullchain)
+    matrix(fullchain[-removethese,], ncol = Nvar)
+}
